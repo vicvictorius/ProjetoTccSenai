03fae1d51ab855ce8c4e945f832e8f7f2b1aa638
nine <119760801+vicvictorius@users.noreply.github.com>
2023-09-01 15:22:48 -0300

    Add files via upload

---
+++ Login-In-WPF-MVVM-C-Sharp-and-SQL-Server-main/WPF-LoginForm/Repositories/RepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Markup;
+using System.Windows;
+
+namespace WPF_LoginForm.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            _connectionString = "Server=VICTOR\\SQLEXPRESS; Database=MVVMLoginDb; Integrated Security=true";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
